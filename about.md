e5f6a33a98c481f2f4cf1cd8ffaa17aeba823a13
Luka S <lukastan93@gmail.com>
2020-07-05 20:22:23 +0400

    First draft ported to Jekyll

---
+++ about.md
@@ -0,0 +1,8 @@
+---
+layout: about
+title: About Me
+---
+<div class="about-container">
+    <h1 class="about-heading">I am a Dubai-based web designer, developer, and digital marketer.</h1>
+    <h2>Come at me with your most exciting idea and I'll make it happen.</h2>
+</div>
---
+++ assets/js/homepage-heading.js
@@ -0,0 +1,12 @@
+document.querySelector('body').addEventListener('mousemove', titlemove);
+function titlemove(){
+  let title = document.querySelectorAll('.title-container');
+  title.forEach(function(title){
+    let x = title.getBoundingClientRect().left + (title.clientWidth / 2);
+    let y = title.getBoundingClientRect().top + (title.clientHeight / 2);
+    let radian = Math.atan2(event.pageX - x, event.pageY - y);
+    let yMove = Math.cos(radian)*8;
+    let xMove = Math.sin(radian)*8;
+    title.style.transform = "translate(" + xMove + "px , " + yMove + "px )";
+  })
+}
